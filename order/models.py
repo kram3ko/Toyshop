0f219c4103f3803c27c8aac103a431a9c1d87be3
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-04-03 17:07:17 +0300

    add field total price and absolute url to order model

--- order/models.py
+++ order/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
 from cart.models import Cart
 
@@ -22,7 +23,7 @@ class Order(models.Model):
     cart = models.OneToOneField(
         to=Cart, on_delete=models.SET_NULL, null=True, blank=True
     )
-
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     deliver_to = models.CharField(max_length=255)
     delivery_method = models.CharField(
         max_length=20, choices=DeliveryMethod.choices
@@ -31,6 +32,10 @@ class Order(models.Model):
         max_length=20, choices=PaymentMethod.choices
     )
     recipient = models.CharField(max_length=255)
+
+    def get_absolute_url(self):
+        return reverse("order:order-detail", kwargs={"pk": self.pk})
+
 
     def __str__(self):
         return f"Order #{self.id} for {self.recipient}"
