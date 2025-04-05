453f2f362ed707e13825e87101cf23b9fa7d3e41
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-04-05 10:13:30 +0300

    Configure order view.

--- order/views.py
+++ order/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
 
-# Create your views here.
+from cart.models import Cart
+from order.models import Order
+
+app_name = "order"
+
+
+class OrderCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Order
+    fields = "__all__"
+    template_name = "toyshop/order/order_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart, status = Cart.objects.get_or_create(user=self.request.user, is_active=True)
+        context["cart"] = cart
+        return context
+
+    def form_valid(self, form):
+        order = form.save()
+        order.cart.is_active = False
+        order.cart.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+
+class OrderDetailView(generic.DetailView):
+    model = Order
+    template_name = "toyshop/order/order_checkout.html"
