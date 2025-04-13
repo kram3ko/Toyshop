c3e72b8f09b3bd439265d9ce6618fef68630011f
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-04-13 09:13:45 +0300

    add functionality .Delete customer add customer, assign card. Modify templates

--- customers/urls.py
+++ customers/urls.py
@@ -6,15 +6,17 @@ from customers.views import (
     CustomerUpdateView,
     ToyClubAssignView,
     ToyClubCreateView,
-    ToyClubListView,
+    ToyClubListView, CustomerDeleteView, CustomerCreateView,
 )
 
 app_name = "customers"
 urlpatterns = [
+    path("create/", CustomerCreateView.as_view(), name="customer-create"),
     path("list/", CustomersListView.as_view(), name="customers-list"),
     path("<int:pk>/profile/", CustomerProfileView.as_view(), name="profile"),
     path("<int:pk>/update/", CustomerUpdateView.as_view(), name="customer-update"),
     path("toyclub/create/", ToyClubCreateView.as_view(), name="toy-club-create"),
     path("toyclub/list/", ToyClubListView.as_view(), name="toy-club-list"),
     path("toyclub/<int:pk>/assign", ToyClubAssignView.as_view(), name="toy-assign"),
+    path("<int:pk>/delete/", CustomerDeleteView.as_view(), name="customer-delete")
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -7,9 +8,14 @@ from customers.forms import (
     CustomerUpdateForm,
     ToyClubCreationForm,
 )
-from customers.models import ToyClub, User
+from customers.models import ToyClub
+
+User = get_user_model()
 
 
+class CustomerCreateView(LoginRequiredMixin, generic.CreateView):
+    model = User
+    success_url = reverse_lazy("customers:customers-list")
 class CustomersListView(generic.ListView):
     model = User
     template_name = "toyshop/customers/customer_list.html"
@@ -32,6 +38,12 @@ class CustomerUpdateView(LoginRequiredMixin, generic.UpdateView):
     template_name = "toyshop/customers/customer_form.html"
 
 
+class CustomerDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = User
+    template_name = "toyshop/customers/customer_confirm_delete.html"
+    success_url = reverse_lazy("customers:customers-list")
+
+
 class ToyClubCreateView(LoginRequiredMixin, generic.CreateView):
     model = ToyClub
     form_class = ToyClubCreationForm
@@ -43,11 +55,43 @@ class ToyClubListView(LoginRequiredMixin, generic.ListView):
     model = ToyClub
     template_name = "toyshop/customers/toy_club_list.html"
 
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["club_to_user"] = self.request.POST.get("club_to_user")
+        return context
+
+    def get_queryset(self):
+        queryset = ToyClub.objects.all()
+        user_id = self.request.POST.get("club_to_user")
+        if user_id:
+            queryset = queryset.filter(user_id__isnull=True)
+        return queryset
+
 
 class ToyClubAssignView(LoginRequiredMixin, generic.UpdateView):
     model = ToyClub
-    fields = [
-        "user",
-    ]
+    fields = ["user"]
     template_name = "toyshop/customers/toy_club_assign.html"
     success_url = reverse_lazy("customers:toy-club-list")
+
+    def post(self, request, *args, **kwargs):
+        if "confirm" in request.POST:
+            return super().post(request, *args, **kwargs)
+
+        self.object = self.get_object()
+        return self.render_to_response(self.get_context_data())
+
+    def form_valid(self, form):
+        user_id = self.request.POST.get("club_to_user")
+        if user_id:
+            form.instance.user_id = user_id
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        club_to_user = self.request.GET.get("club_to_user") or self.request.POST.get("club_to_user")
+        context["club_to_user"] = club_to_user
+        return context
