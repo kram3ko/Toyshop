178d3f2c2c8b22e72b6e06294581c63ca298589d
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-04-25 13:25:14 +0300

    Refactor models and migrations for Cart, Order, and Wishlist; update field types and relationships

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "toys_shop.settings.prod")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "toys_shop.settings.dev")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
